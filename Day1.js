fbd189a99677266f0d833b2288f99f359e1b8613
mannu chaurasiya <mannuchaurasiya36@gmail.com>
2024-07-24 03:07:03 +0530

    Day1-Work

--- Day1.js
+++ Day1.js
@@ -1,2 +1,46 @@
+
+
+// TODO : Activity 1 
 var1 = 34
 console.log(var1);
+
+let str = "aman";
+console.log(str);
+
+// TODO: ACTIVITY 2
+
+const dec = true;
+console.log(dec);
+
+// TODO: Activity 3: 
+
+var x = Number(1);
+
+console.log(typeof(x));
+
+var y = String("Mannu");
+var z = Boolean(1);
+var a = Object({x : 1});
+var b = Array(2);
+
+console.log(typeof(y));
+console.log(typeof(z));
+console.log(typeof(a));
+console.log(typeof(b));
+
+
+// TODO: ACTIVITY 4
+
+let op = 1;
+console.log(op)
+
+op = 5;
+console.log(op);
+
+// TODO: ACTIVITY 5
+
+const re = 5;
+console.log(re);
+re = 7
+console.log(re); 
+// output for this is Assignment to Constant is not possiible
