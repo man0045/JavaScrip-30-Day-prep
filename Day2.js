2f22d80cdd28fc5db175286dc2e2b8be4549d628
mannu chaurasiya <mannuchaurasiya36@gmail.com>
2024-07-24 13:18:20 +0530

    Work of day2

---
+++ Day2.js
@@ -0,0 +1,106 @@
+// TODO: Activit 1
+
+const a = 9;
+const b = 23;
+
+resa = a+b;
+resd = Math.abs(a-b);
+resm = a*b;
+resdi = a/b;
+console.log(resa);
+console.log(resd);
+console.log(resm); 
+console.log(resdi);
+
+// TODO: Activity 2
+
+let x = 6;
+x+=1;
+console.log(x);
+x-=1;
+console.log(x);
+
+// TODO: Activity 3
+
+
+let bw= 34;
+let aw = 56;
+
+if(aw > bw){
+ console.log(aw);
+}
+if(aw < bw){
+ console.log(aw);
+}
+
+if(aw == bw){
+ console.log("Equal to both")
+}else{
+ console.log("Not Equal to each other")
+}
+
+let p = 45;
+var q = String("45");
+if(p === q){
+ console.log("Both data types and values are equal");
+}
+else{
+ console.log("Only value is equal")
+}
+
+
+//TODO: Activity 4
+
+if( aw > 50 &&  x < 50){
+ console.log("Yes it is following the condition");
+}
+else{
+ console.log("Nothing such types of things exists");
+}
+if(aw > 50 || a < 45){
+ console.log("You are correct ");
+}
+else{
+ console.log("No-Please")
+}
+
+const prex = Object({
+ name: "Mannu",
+ preprocess : 45,
+ hang: null
+
+})
+
+if(!prex.hang){
+ console.log("null value assigned")
+}
+else{
+ console.log("Every thing working well")
+}
+// TODO: Activity 5
+
+var as = 5;
+console.log((as  < 0) ? -1: 1)
+
+//TODO:  Feautured request ans 
+
+function add(a, b){
+ return a+b;
+}
+
+function sub(a, b){
+ return a-b;
+}
+
+function mul(a, b){
+ return a*b;
+}
+function div(a, b){
+ return a/b;
+}
+
+console.log(add(45, 89));
+console.log(sub(45, 66));
+console.log(mul(45, 66));
+console.log(div(45, 66));
+
