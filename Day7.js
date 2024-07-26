6149953c15ac2e8aa42aaba52f161b5fee26ee89
mannu chaurasiya <mannuchaurasiya36@gmail.com>
2024-07-27 02:42:53 +0530

    Adding Activity1 of Day7

---
+++ Day7.js
@@ -0,0 +1,21 @@
+// TODO: Activity 1
+
+const book = {
+ name: "Class of 83",
+ Author: "Mr Garvit tyagi",
+ year: "1990"
+}
+
+console.log(book);
+
+const newe = book.name;
+const Auth = book.Author;
+console.log(newe, Auth);
+
+
+
+
+
+
+// TODO: Activit2
+
