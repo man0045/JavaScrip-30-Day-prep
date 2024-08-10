8459879c99d5fbdda20035370612475871421f6b
mannu chaurasiya <mannuchaurasiya36@gmail.com>
2024-08-11 00:31:17 +0530

    Updatinng commits

---
+++ hoisting.js
@@ -0,0 +1,13 @@
+name = "aman";
+let name;
+
+
+// Hoisting : it is the process of declaring variable without intializing or assigning any value inside the variable
+// closure
+// let var const
+// why nodejs is single threaded
+// hooks is the ways of acceptng and assigning state and features of another noted state
+// props means properties getting the properties from one component to another component
+// state
+// reactrouter
+
