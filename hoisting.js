56f19aa0d14379bf91970eb86a37f83e51037dc9
mannu chaurasiya <mannuchaurasiya36@gmail.com>
2024-08-12 01:55:51 +0530

    Adding Some interview questions

--- hoisting.js
+++ hoisting.js
@@ -10,4 +10,13 @@ let name;
 // props means properties getting the properties from one component to another component
 // state
 // reactrouter
-
+// Why React JavaScript is unidirectional:: Ans::  because it move forward from parent component to child component
+ // AND state makes more predicatable and reason about
+ // LifeCycle steps in Reactjs:::  1.Initializing 2.Mounting 3. Updating and 4.Unmounting
+ // Multiple ways to optimize react application:: Ans:: By using Hooks like Usememo, useState ,
+  // by applying ServerSide rendering techniques and by code splitting and React.lazy suspense Avoid anomynous function in js
+  // Is reactjs uses functional components or classcomponrnt:: Ans:: Reactjs uses functional componenets because
+  // of is readibility usability performance and power of hooks, class components are used as a legacy patterns
+  //Higher order fuction :: function inside another function is used for such
+  // Callback function
+  // Strict Mode in react js
